8e60df2257b2b23eaa67dc8f8a4d69ee35619584
gbalduzz <gbalduzz@itp.phys.ethz.ch>
2018-10-29 22:12:37 +0100

    Harder test.

--- test/algorithms/serial_connected_components_test.cpp
+++ test/algorithms/serial_connected_components_test.cpp
@@ -2,22 +2,38 @@
 
 #include <gtest/gtest.h>
 
-TEST(SerialConnectedComponentsTest, All) {
-  const int n = 5;
-  std::vector<graph::Edge> edges{std::make_pair(1, 0), std::make_pair(2, 1), std::make_pair(2, 0),
-                                 std::make_pair(4, 3)};
+void performTest(int n, const std::vector<graph::Edge>& edges, const std::vector<int>& expected);
 
+//TEST(SerialConnectedComponentsTest, Simple) {
+//  const int n = 5;
+//  const std::vector<graph::Edge> edges{std::make_pair(1, 0), std::make_pair(2, 1),
+//                                       std::make_pair(2, 0), std::make_pair(4, 3)};
+//  const std::vector<int> expected{0, 0, 0, 1, 1};
+//
+//  performTest(n, edges, expected);
+//}
+
+TEST(SerialConnectedComponentsTest, LessSimple) {
+  const int n = 12;
+  const std::vector<graph::Edge> edges{
+      std::make_pair(8, 2),  std::make_pair(5, 1), std::make_pair(3, 0),
+      std::make_pair(6, 3),  std::make_pair(6, 0), std::make_pair(5, 4),
+      std::make_pair(11, 6), std::make_pair(7, 4), std::make_pair(10, 9)};
+
+  const std::vector<int> expected{0, 1, 2, 0, 1, 1, 0, 1, 2, 3, 3, 0};
+
+  performTest(n, edges, expected);
+}
+
+void performTest(int n, const std::vector<graph::Edge>& edges, const std::vector<int>& expected) {
   auto forest = algorithms::serialConnectedComponents(n, edges);
 
   auto are_connected = [&](int i, int j) {
     return forest.representative(i) == forest.representative(j);
   };
+  auto expect_connected = [&](int i, int j) { return expected[i] == expected[j]; };
 
-  for (int i = 0; i <= 2; ++i)
-    EXPECT_TRUE(are_connected(0, i));
-
-  EXPECT_FALSE(are_connected(0, 3));
-
-  for (int i = 3; i <= 4; ++i)
-    EXPECT_TRUE(are_connected(3, i));
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j)
+      EXPECT_EQ(expect_connected(i, j), are_connected(i, j));
 }
